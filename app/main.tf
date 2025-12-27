a355db947a287620f9d7e69d566989b3b5716c09
narlechitane38200 <narlechitane38200@gmail.com>
2025-12-27 22:50:39 +0100

    modify

--- app/main.tf
+++ app/main.tf
@@ -7,14 +7,6 @@ terraform {
     }
   }
   required_version = "1.14.2"
-
-#  backend "s3" {
-#    bucket = "terraform-backend-radouane"
-#    key    = "demo_tfstate"
-#    region = "us-east-1"
-#    shared_credentials_files = [ "../.secrets/credentials" ]
-#    profile = "eazytraining-tp3"
-#  }
 }
 
 
@@ -40,10 +32,10 @@ module "myvpc" {
   vpc_cidr = "172.31.0.0/16"
 
   public_subnets = [
-    { cidr = "172.31.1.0/24", az = "eu-east-1a" },
+    { cidr = "172.31.1.0/24", az = "us-east-1a" },
   ]
    private_subnets = [
-    { cidr = "172.31.101.0/24", az = "eu-west-1a" },
+    { cidr = "172.31.101.0/24", az = "us-west-1a" },
   ]
 }
 
